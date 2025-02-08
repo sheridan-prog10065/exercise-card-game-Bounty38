2c0a918c70565d8c1ce78928a8a1d0c40990a88b
Bounty <kislicyn.sema@yandex.ru>
2025-02-07 21:59:45 -0500

    Implement Card, CardDeck, and CardGame classes with properties and methods for game logic

--- CardGame_Interactive/CardGameInteractive/Score.cs
+++ CardGame_Interactive/CardGameInteractive/Score.cs
@@ -8,5 +8,20 @@ public struct Score
     
     //Number of rounds won by the House
     private int _houseScore;
+
+    public int PlayerScore
+    {
+        get
+        {
+            return _playerScore;
+        }
+    }
     
+    public int HouseScore
+    {
+        get
+        {
+            return _houseScore;
+        }
+    }
 }
